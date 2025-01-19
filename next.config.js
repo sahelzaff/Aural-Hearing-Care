9aea7b61ed52fe86bd3025e5959b0e0397e327cb
Sahel Zaffarulla <sahel@goodrichlogistics.local>
2025-01-19 23:25:00 +0530

    contact page

--- next.config.js
+++ next.config.js
@@ -17,8 +17,7 @@ const nextConfig = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
-  },
-  output: 'export'
+  }
 }
 
 module.exports = nextConfig
